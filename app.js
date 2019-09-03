cb930faa386d94a65fa452a5b4940e0f233d301a
alvinyans <ifeanyiuyanna@gmail.com>
2019-09-03 22:46:45 +0100

    changes

--- app.js
+++ app.js
@@ -12,6 +12,15 @@ function ContactListApp() {
 		favourites : []
 	};
 
+function alphabeticalOrder(arr) {
+  // Add your code below this line
+  return arr.sort(function(a,b) {
+    return a > b;
+  });
+  // Add your code above this line
+}
+alphabeticalOrder(["a", "d", "c", "a", "z", "g"]);
+
 	//@Custom event names
 	this.events = {
 		newContact    : 'newContact',
